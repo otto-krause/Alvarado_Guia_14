815f5cb20566bd07e84cbaa49e946c81118c8d92
JordiAlvarado <86363800+JordiAlvarado@users.noreply.github.com>
2021-11-17 19:38:58 -0300

    Add files via upload

---
+++ Ejercicio_4.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main()
+{
+
+    int Matriz[14][4]={0}, MT=0, L=0, V=0, C=0;
+    for (L=1; L<=3; L++)
+    {
+        printf("Linea %d\n", L);
+        printf("Ingrese el numero de coche y luego su recaudacion\n");
+        for (V=1; V<=13; V++)
+        {   
+            printf("Coche: ");
+            scanf("%d", &C);
+            
+            if (C<=0)
+            {
+                V=13;
+            }
+            else
+            {
+                printf("Recaudacion: ");
+                scanf("%d", &Matriz[C][L]);
+            }
+        }
+        system("cls");
+    }
+
+    for (L=1; L<=3; L++)
+    {
+        printf("\nLas recaudaciones de los coches de la linea %d fueron:\n", L);
+        for (V=1; V<=13; V++)
+        {
+            if (Matriz[V][L]==0)
+            {
+            }
+            else
+            {
+               printf("Coche %d: %d\n", V, Matriz[V][L]);
+            }
+            Matriz[0][L]=Matriz[0][L]+Matriz[V][L];
+        }
+    }
+
+    printf("\nLas recaudaciones totales de cada linea fueron:\n");
+    for (L=1; L<=3; L++)
+    {
+        printf("Linea %d: %d\n", L, Matriz[0][L]);
+        MT=MT+Matriz[0][L];
+    }
+
+    printf("\nLa recaudacion total de las tres lineas fue %d", MT);
+}
